e039cac9a31e846dbf2fb8b3320583dea04c4ae5
Sudha SK <sskumar4@ncsu.edu>
2020-08-08 14:57:54 -0400

    code cleanup for eTrackerclass.js

--- eTrackerClass.js
+++ eTrackerClass.js
@@ -1,64 +1,34 @@
 const mysql = require("mysql");
-
 class Database {
-
     constructor(config) {
-
-        this.connection = mysql.createConnection(config);
-
-    }
-
-    // new Promise((_, reject) => reject(new Error('woops'))).
-    //     // Prints "caught woops"
-    // catch(error => { console.log('caught', error.message); });
-
+            this.connection = mysql.createConnection(config);
+        }
+        // new Promise((_, reject) => reject(new Error('woops'))).
+        //     // Prints "caught woops"
+        // catch(error => { console.log('caught', error.message); });
     query(sql, args) {
-
         return new Promise((resolve, reject) => {
-
             this.connection.query(sql, args, (err, rows) => {
-
                 if (err) {
-
                     console.log(err.sql);
-
                     console.log("");
-
                     return reject(err);
-
                 }
-
                 resolve(rows);
-
             });
-
         }).catch(error => {
-            console.log('Remove referencing records from other tables before this can be deleted')
-                //console.log('caught', error.message);
-
+            console.log('Query exception occured');
         });
-
     };
-
     close() {
-
         return new Promise((resolve, reject) => {
-
             this.connection.end(err => {
-
                 if (err)
-
                     return reject(err);
-
                 resolve();
-
             });
-
         });
-
     }
-
 }
-
 
 module.exports = Database;
